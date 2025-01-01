0f984a0e0fd0995f9ad69161efe205a3e72cba6e
Sumit Bisht <sumitbisht@Sumits-MacBook-Air.local>
2025-01-01 23:53:09 +0530

    added a new component

---
+++ src/components/RecentSearchResults.tsx
@@ -0,0 +1,19 @@
+const RecentSearchResults = () => {
+  return (
+    <div className="flex flex-col absolute bg-orange-100 h-[200px] w-[600px] top-full right-0 rounded-xl mt-1 p-4">
+      {/* <div className="relative flex">
+        <p className="mx-auto px-2 italic text-xs bg-orange-100 z-10">
+          Recent Searches
+        </p>
+        <div className="border rounded-full absolute w-full top-1/2 border-black"></div>
+      </div> */}
+
+      <div>
+        <h1>1</h1>
+        <h1>2</h1>
+      </div>
+    </div>
+  );
+};
+
+export default RecentSearchResults;
